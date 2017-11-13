ba9fda9683d247d6e46d107c46496575191a6072
Ruben Javier Jimenez <rjjimenez@paradigmadigital.com>
2017-11-13 20:09:45 +0100

    creado test de prueba y añadido al specRunner.html

---
+++ spec/testSpec.js
@@ -0,0 +1,7 @@
+describe("Login", function() {
+    describe("Prueba", function() {
+        it("should be true", function() {
+            expect("true").toEqual("true");
+        });
+    });
+});
