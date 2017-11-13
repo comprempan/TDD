05514c25d8c88010081be3c94707751cde692973
Ruben Javier Jimenez <rjjimenez@paradigmadigital.com>
2017-11-13 20:36:39 +0100

    código para validar email vacio

--- src/app.js
+++ src/app.js
@@ -8,4 +8,8 @@ function login(form) {
     if(!form.name || form.name === "") {
         formErrors.name = "El usuario no puede estar vacío";
     }
+
+    if(!form.email || form.email === "") {
+        formErrors.email = "El email no puede estar vacío";
+    }
 }
