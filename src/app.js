5b7c29f675f31c0a3cd8c7b9992e4c58a08d44bd
Ruben Javier Jimenez <rjjimenez@paradigmadigital.com>
2017-11-13 21:10:58 +0100

    refactorizado el código para que pasen los test. aun así no pasan

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
-var formErrors = {
-    name: null,
-    email: null,
-    password: null
+var validForm = {
+    errors: {
+        name: null,
+        email: null,
+        password: null
+    },
+    valid: {
+        name: null,
+        email: null,
+        password: null
+    }
 };
 
 function login(form) {
@@ -9,22 +16,40 @@ function login(form) {
     var passRegex = /(?=^.{7,}$)((?=.*\d)|(?=.*\W+))(?=.*[A-Z])(?=.*[a-z]).*$/;
 
     if(!form.name || form.name === "") {
-        formErrors.name = "El usuario no puede estar vacío";
+        validForm.errors.name = "El usuario no puede estar vacío";
+        validForm.valid.name = false;
+    } else {
+        validForm.errors.name = null;
+        validForm.valid.name = true;
     }
 
     if(!form.email || form.email === "") {
-        formErrors.email = "El email no puede estar vacío";
+        validForm.errors.email = "El email no puede estar vacío";
+        validForm.valid.email = false;
     } else if(!emailRegex.test(form.email)) {
-        formErrors.email = "El email no es válido";
+        validForm.errors.email = "El email no es válido";
+        validForm.valid.email = false;
+    } else {
+        validForm.errors.email = null;
+        validForm.valid.email = true;
     }
 
     if(!form.password || form.password === "") {
-        formErrors.password = "La contraseña no puede estar vacía";
+        validForm.errors.password = "La contraseña no puede estar vacía";
+        validForm.valid.password = false;
     } else if(!passRegex.test(form.password)) {
-        formErrors.password = "La contraseña no es válida";
+        validForm.errors.password = "La contraseña no es válida";
+        validForm.valid.password = false;
+    } else {
+        validForm.errors.password = null;
+        validForm.valid.password = true;
+    }
+
+    if(validForm.valid.name && validForm.valid.email && validForm.valid.password) {
+        submitForm();
     }
 }
 
 function submitForm() {
-    
+
 }
