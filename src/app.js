a9378f2ee3e96632a8c4959a3c02502f48b5f278
Ruben Javier Jimenez <rjjimenez@paradigmadigital.com>
2017-11-13 20:40:22 +0100

    codigo para validar email

--- src/app.js
+++ src/app.js
@@ -5,11 +5,15 @@ var formErrors = {
 };
 
 function login(form) {
+    var emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+
     if(!form.name || form.name === "") {
         formErrors.name = "El usuario no puede estar vacío";
     }
 
     if(!form.email || form.email === "") {
         formErrors.email = "El email no puede estar vacío";
+    } else if(!emailRegex.test(form.email)) {
+        formErrors.email = "El email no es válido";
     }
 }
