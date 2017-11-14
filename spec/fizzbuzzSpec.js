20f482a84d3c087685851fa3a0ad378f3947be30
Ruben Javier Jimenez <rjjimenez@paradigmadigital.com>
2017-11-14 09:03:23 +0100

    añadida primera especificacion en los tes 'Devuelve Fizz si el número es divisible por 3'

--- spec/fizzbuzzSpec.js
+++ spec/fizzbuzzSpec.js
@@ -1,3 +1,6 @@
 describe("FizzBuzz", function() {
-    
+    it( 'should return Fizz for 3', function() {
+        var result = FizzBuzz.check( 3 );
+        expect( result ).toEqual( 'Fizz' );
+    });
 });
