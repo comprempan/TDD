3d440471c8c1ba4bf0a7bbbae627667279eb04dd
Ruben Javier Jimenez <rjjimenez@paradigmadigital.com>
2017-11-14 09:10:34 +0100

    añadida segunda especificacion en los tests 'Devuelve Buzz si el número es divisible por 5'

--- spec/fizzbuzzSpec.js
+++ spec/fizzbuzzSpec.js
@@ -1,6 +1,11 @@
 describe("FizzBuzz", function() {
-    it( 'should return Fizz for 3', function() {
-        var result = FizzBuzz.check( 3 );
-        expect( result ).toEqual( 'Fizz' );
+    it('should return Fizz for 3', function() {
+        var result = FizzBuzz.check(3);
+        expect(result).toEqual('Fizz');
+    });
+
+    it('should return Fizz for 9', function() {
+        var result = FizzBuzz.check(9);
+        expect(result).toEqual('Fizz');
     });
 });
