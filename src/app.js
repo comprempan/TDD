abd1ebfb55d7b9eb0a1c743e433420ca01e9c9c8
Ruben Javier Jimenez <rjjimenez@paradigmadigital.com>
2017-11-14 17:03:09 +0100

    refactor sin tocar los tests

--- src/app.js
+++ src/app.js
@@ -12,44 +12,76 @@ var validForm = {
 };
 
 function login(form) {
-    var emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-    var passRegex = /(?=^.{7,}$)((?=.*\d)|(?=.*\W+))(?=.*[A-Z])(?=.*[a-z]).*$/;
-
-    if(!form.name || form.name === "") {
-        validForm.errors.name = "El usuario no puede estar vacío";
-        validForm.valid.name = false;
-    } else {
-        validForm.errors.name = null;
-        validForm.valid.name = true;
-    }
-
-    if(!form.email || form.email === "") {
-        validForm.errors.email = "El email no puede estar vacío";
-        validForm.valid.email = false;
-    } else if(!emailRegex.test(form.email)) {
-        validForm.errors.email = "El email no es válido";
-        validForm.valid.email = false;
-    } else {
-        validForm.errors.email = null;
-        validForm.valid.email = true;
-    }
-
-    if(!form.password || form.password === "") {
-        validForm.errors.password = "La contraseña no puede estar vacía";
-        validForm.valid.password = false;
-    } else if(!passRegex.test(form.password)) {
-        validForm.errors.password = "La contraseña no es válida";
-        validForm.valid.password = false;
-    } else {
-        validForm.errors.password = null;
-        validForm.valid.password = true;
-    }
-
-    if(validForm.valid.name && validForm.valid.email && validForm.valid.password) {
+    if(isValidForm(form)) {
         submitForm();
     }
 }
 
-function submitForm() {
+function submitForm() {}
 
+
+function isValidForm(form) {
+    var valid = false;
+    validForm.valid.name = validateName(form.name);
+    validForm.valid.email = validateEmail(form.email);
+    validForm.valid.password = validatePassword(form.password);
+
+    if(validForm.valid.name && validForm.valid.email && validForm.valid.password) {
+        valid = true;
+    }
+
+    return valid;
 }
+
+function validateName(name) {
+    var valid = false;
+    if(!name || name === "") {
+        validForm.errors.name = "El usuario no puede estar vacío";
+    } else {
+        validForm.errors.name = null;
+        valid = true;
+    }
+
+    return valid;
+}
+
+function validateEmail(email) {
+    var valid = false;
+    var emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    if(!email || email === "") {
+        validForm.errors.email = "El email no puede estar vacío";
+    } else if(!emailRegex.test(email)) {
+        validForm.errors.email = "El email no es válido";
+    } else {
+        validForm.errors.email = null;
+        valid = true;
+    }
+
+    return valid;
+}
+
+
+function validatePassword(password) {
+    var valid = false;
+    var passRegex = /(?=^.{7,}$)((?=.*\d)|(?=.*\W+))(?=.*[A-Z])(?=.*[a-z]).*$/;
+
+    if(!password || password === "") {
+        validForm.errors.password = "La contraseña no puede estar vacía";
+    } else if(!passRegex.test(password)) {
+        validForm.errors.password = "La contraseña no es válida";
+    } else {
+        validForm.errors.password = null;
+        valid = true;
+    }
+
+    return valid;
+}
+
+
+/*function login(formData) {
+    if(ValidateForm.isValid(formData)) {
+        submitForm();
+    }
+}
+
+function submitForm() {}*/
